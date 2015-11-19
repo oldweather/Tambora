699dd2b1aeac9429a39880c60bf338476b026129
Philip Brohan <philip@brohan.org>
2015-11-19 15:28:44 +0000

    Added preliminay 364 to correlations.

--- plots/station_anomalies_correlation.daily.R
+++ plots/station_anomalies_correlation.daily.R
@@ -11,6 +11,8 @@ bad.stations<-c(1,5,6,9,10,11,12,14,15,17,21,23,29,30,31,32,34,35,37,38,39,40,41
 
 # Load station data and Reanalysis data
 load('../Yuri/0_all_station_and_travel_pressure_data.Rdata')
+load('../Reanalysis.364.Rdata')
+Dates.364<-Dates
 load('../Reanalysis.366.Rdata')
 Dates.366<-Dates
 load('../Reanalysis.Rdata')
@@ -48,6 +50,11 @@ cor.station<-function(station) { # station is an integer in range 1:51
     v356$t2m.spread<-rep(NA,length(Dates))
     v356$prmsl.mean<-rep(NA,length(Dates))
     v356$prmsl.spread<-rep(NA,length(Dates))
+    v364<-list()
+    v364$t2m.mean<-rep(NA,length(Dates))
+    v364$t2m.spread<-rep(NA,length(Dates))
+    v364$prmsl.mean<-rep(NA,length(Dates))
+    v364$prmsl.spread<-rep(NA,length(Dates))
     v366<-list()
     v366$t2m.mean<-rep(NA,length(Dates))
     v366$t2m.spread<-rep(NA,length(Dates))
@@ -64,6 +71,8 @@ cor.station<-function(station) { # station is an integer in range 1:51
        v355$t2m.spread[i]<-Dates[[i]]$station[[station]]$v355$t2m.spread
        v356$t2m.mean[i]<-Dates[[i]]$station[[station]]$v356$t2m.mean-273.15
        v356$t2m.spread[i]<-Dates[[i]]$station[[station]]$v356$t2m.spread
+       v364$t2m.mean[i]<-Dates.364[[i]]$station[[station]]$v364$t2m.mean-273.15
+       v364$t2m.spread[i]<-Dates.364[[i]]$station[[station]]$v364$t2m.spread
        v366$t2m.mean[i]<-Dates.366[[i]]$station[[station]]$v366$t2m.mean-273.15
        v366$t2m.spread[i]<-Dates.366[[i]]$station[[station]]$v366$t2m.spread
        v354$prmsl.mean[i]<-Dates[[i]]$station[[station]]$v354$prmsl.mean/100
@@ -73,9 +82,11 @@ cor.station<-function(station) { # station is an integer in range 1:51
        v355$prmsl.spread[i]<-Dates[[i]]$station[[station]]$v355$prmsl.spread/100
        v356$prmsl.mean[i]<-Dates[[i]]$station[[station]]$v356$prmsl.mean/100
        v356$prmsl.spread[i]<-Dates[[i]]$station[[station]]$v356$prmsl.spread/100
+       v364$prmsl.mean[i]<-Dates.364[[i]]$station[[station]]$v364$prmsl.mean/100
+       v364$prmsl.spread[i]<-Dates.364[[i]]$station[[station]]$v364$prmsl.spread/100
        v366$prmsl.mean[i]<-Dates.366[[i]]$station[[station]]$v366$prmsl.mean/100
        v366$prmsl.spread[i]<-Dates.366[[i]]$station[[station]]$v366$prmsl.spread/100
-    }
+   }
     t2m<-strip.diurnal(t2m,dates)
     v354$t2m.mean<-strip.diurnal(v354$t2m.mean,dates)
     v354$t2m.spread<-strip.diurnal(v354$t2m.spread,dates)
@@ -84,6 +95,8 @@ cor.station<-function(station) { # station is an integer in range 1:51
     v355$t2m.spread<-strip.diurnal(v355$t2m.spread,dates)
     v356$t2m.mean<-strip.diurnal(v356$t2m.mean,dates)
     v356$t2m.spread<-strip.diurnal(v356$t2m.spread,dates)
+    v364$t2m.mean<-strip.diurnal(v364$t2m.mean,dates)
+    v364$t2m.spread<-strip.diurnal(v364$t2m.spread,dates)
     v366$t2m.mean<-strip.diurnal(v366$t2m.mean,dates)
     v366$t2m.spread<-strip.diurnal(v366$t2m.spread,dates)
     # Make anomalies
@@ -91,11 +104,13 @@ cor.station<-function(station) { # station is an integer in range 1:51
     v354$t2m.mean<-v354$t2m.mean-v354$t2m.normal
     v355$t2m.mean<-v355$t2m.mean-v354$t2m.normal
     v356$t2m.mean<-v356$t2m.mean-v354$t2m.normal
+    v364$t2m.mean<-v364$t2m.mean-v354$t2m.normal
     v366$t2m.mean<-v366$t2m.mean-v354$t2m.normal
     prmsl<-prmsl-v354$prmsl.normal
     v354$prmsl.mean<-v354$prmsl.mean-v354$prmsl.normal
     v355$prmsl.mean<-v355$prmsl.mean-v354$prmsl.normal
     v356$prmsl.mean<-v356$prmsl.mean-v354$prmsl.normal
+    v364$prmsl.mean<-v364$prmsl.mean-v354$prmsl.normal
     v366$prmsl.mean<-v366$prmsl.mean-v354$prmsl.normal
 
 
@@ -103,18 +118,21 @@ cor.station<-function(station) { # station is an integer in range 1:51
     at.354<-cor(v354$t2m.mean,t2m,use='na.or.complete',method='pearson')
     slp.356<-cor(v356$prmsl.mean,prmsl,use='na.or.complete',method='pearson')
     at.356<-cor(v356$t2m.mean,t2m,use='na.or.complete',method='pearson')
+    slp.364<-cor(v364$prmsl.mean,prmsl,use='na.or.complete',method='pearson')
+    at.364<-cor(v364$t2m.mean,t2m,use='na.or.complete',method='pearson')
     slp.366<-cor(v366$prmsl.mean,prmsl,use='na.or.complete',method='pearson')
     at.366<-cor(v366$t2m.mean,t2m,use='na.or.complete',method='pearson')
 
-    cat(sprintf("| %-20s | %4.2f | %4.2f | %4.2f | %4.2f | %4.2f | %4.2f |\n",PP[[station]]$Station[1],
-     slp.354,slp.356,slp.366,at.354,at.356,at.366),
+    cat(sprintf("| %-20s | %4.2f | %4.2f | %4.2f | %4.2f | %4.2f | %4.2f | %4.2f | %4.2f |\n",
+     PP[[station]]$Station[1],
+     slp.354,slp.356,slp.364,slp.366,at.354,at.356,at.364,at.366),
     file='correlations.md',append=TRUE)
 }
 
 cat("Stations with good data\n\n",file='correlations.md',append=FALSE)
-cat("| Station  | SLP 3.5.4 | SLP 3.5.6 | SLP 3.6.6 | AT 3.5.4 | AT 3.5.6 | AT 3.6.6 |\n",
+cat("| Station  | SLP 3.5.4 | SLP 3.5.6 | SLP 3.6.4 | SLP 3.6.6 | AT 3.5.4 | AT 3.5.6 | AT 3.6.4 | AT 3.6.6 |\n",
     file='correlations.md',append=TRUE)
-cat("| :------- |:---------:|:---------:|:--------:|:--------:|:--------:|:--------:|\n",
+cat("| :------- |:---------:|:---------:|:--------:|:--------:|:--------:|:--------:|:--------:|:--------:|\n",
     file='correlations.md',append=TRUE)
 
 for(station in good.stations) {
@@ -122,9 +140,9 @@ for(station in good.stations) {
 }
 
 cat("\nOther stations\n\n",file='correlations.md',append=TRUE)
-cat("| Station  | SLP 3.5.4 | SLP 3.5.6 | SLP 3.6.6 | AT 3.5.4 | AT 3.5.6 | AT 3.6.6 |\n",
+cat("| Station  | SLP 3.5.4 | SLP 3.5.6 | SLP 3.6.4 | SLP 3.6.6 | AT 3.5.4 | AT 3.5.6 | AT 3.6.4 | AT 3.6.6 |\n",
     file='correlations.md',append=TRUE)
-cat("| :------- |:---------:|:---------:|:--------:|:--------:|:--------:|:--------:|\n",
+cat("| :------- |:---------:|:---------:|:--------:|:--------:|:--------:|:--------:|:--------:|:--------:|\n",
     file='correlations.md',append=TRUE)
 
 for(station in bad.stations) {
