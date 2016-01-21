b7c85ff10f9c5fe433e7b6c9b73f8afea71949f2
Philip Brohan <philip@brohan.org>
2016-01-21 17:19:01 +0000

    Added publication increments figure.  Also added European region video plot file (previously omitted).

---
+++ plots/increments_panel.R
@@ -0,0 +1,100 @@
+#  Calculate reanalysis increments at each station
+#   plot them for 354 and 366.
+
+library(grid)
+library(chron)
+
+# Divide stations into high-quality and 'other'
+good.stations<-c(2,3,4,7,8,13,16,18,19,20,22,24,25,26,27,28,33,36,46,47,49,50,51)
+bad.stations<-c(1,5,6,9,10,11,12,14,15,17,21,23,29,30,31,32,34,35,37,38,39,40,41,42,43,44,45,48)
+
+# Load station data and Reanalysis data
+load('../Yuri/0_all_station_and_travel_pressure_data.Rdata')
+load('../Reanalysis.Rdata')
+Dates.356<-Dates
+load('../Reanalysis.366.Rdata')
+Dates.366<-Dates
+load('../Reanalysis.354.fd.Rdata')
+Dates.354.fd<-Dates
+load('../Reanalysis.356.fd.Rdata')
+Dates.356.fd<-Dates
+
+cor.station<-function(station) { # station is an integer in range 1:51
+
+    # Reformat the station data into lists
+    dates<-rep(Dates[[1]]$date,length(Dates))
+    t2m<-rep(NA,length(Dates))
+    prmsl<-rep(NA,length(Dates))
+    v354<-list()
+    v354$t2m.mean<-rep(NA,length(Dates))
+    v354$t2m.fd<-rep(NA,length(Dates))
+    v354$prmsl.mean<-rep(NA,length(Dates))
+    v354$prmsl.fd<-rep(NA,length(Dates))
+    v356<-list()
+    v356$t2m.mean<-rep(NA,length(Dates))
+    v356$t2m.fd<-rep(NA,length(Dates))
+    v356$prmsl.mean<-rep(NA,length(Dates))
+    v356$prmsl.fd<-rep(NA,length(Dates))
+    for(i in seq_along(Dates)) {
+       dates[i]<-Dates[[i]]$date
+       v354$t2m.mean[i]<-Dates.356[[i]]$station[[station]]$v354$t2m.mean
+       v354$t2m.fd[i]<-Dates.354.fd[[i]]$station[[station]]$v354$t2m.mean
+       v354$prmsl.mean[i]<-Dates.356[[i]]$station[[station]]$v354$prmsl.mean/100
+       v354$prmsl.fd[i]<-Dates.354.fd[[i]]$station[[station]]$v354$prmsl.mean/100
+       v356$t2m.mean[i]<-Dates.356[[i]]$station[[station]]$v356$t2m.mean
+       v356$t2m.fd[i]<-Dates.356.fd[[i]]$station[[station]]$v356$t2m.mean
+       v356$prmsl.mean[i]<-Dates.356[[i]]$station[[station]]$v356$prmsl.mean/100
+       v356$prmsl.fd[i]<-Dates.356.fd[[i]]$station[[station]]$v356$prmsl.mean/100
+    }
+
+
+    slp.354<-sd(v354$prmsl.mean-v354$prmsl.fd,na.rm=TRUE)
+    t2m.354<-sd(v354$t2m.mean-v354$t2m.fd,na.rm=TRUE)
+    slp.356<-sd(v356$prmsl.mean-v356$prmsl.fd,na.rm=TRUE)
+    t2m.356<-sd(v356$t2m.mean-v356$t2m.fd,na.rm=TRUE)
+
+    grid.lines(x=unit(c(0.05,0.4),'native'),
+                y=unit(c(slp.354,slp.356),'native'))
+    grid.points(x=unit(c(0.05,0.4),'native'),
+	    y=unit(c(slp.354,slp.356),'native'),
+	    size=unit(0.01,'npc'),pch=20)
+   return(slp.356)
+
+}
+
+pdf(file="Increments.panel.pdf",
+    width=10/sqrt(2),height=10,family='Helvetica',
+    paper='special',pointsize=18,bg='white')
+
+    pushViewport(viewport(width=1.0,height=1.0,x=0.0,y=0.0,
+                          just=c("left","bottom"),name="Page",clip='off'))
+       pushViewport(plotViewport(margins=c(3,4,0,0)))
+          pushViewport(dataViewport(c(0,1),c(1,6),clip='off'))
+             grid.xaxis(at=c(0.05,0.4),label=c('No forcing','With forcing'),main=T)
+             grid.yaxis(main=T)
+             grid.text('Pressure increment sd (hPa)',x=unit(-3,'lines'),rot=90)
+
+            gp<-gpar(col=rgb(0.7,0.7,0.7,1),fill=rgb(0.7,0.7,0.7,1))
+             with.by.name<-list()
+             for(station in good.stations) {
+                with.by.name[[PP[[station]]$Station.name[1]]]<-cor.station(station)
+             }
+             w<-order(unlist(with.by.name))
+             for(sti in seq_along(with.by.name)) {
+                yp<-0.0+(sti/length(w))*0.8
+                stnname<-names(with.by.name)[w[sti]]
+                if(length(grep('itenice',stnname))>0) stnname<-'Zitenice' # Unprintable
+                grid.text(stnname,
+                  x=unit(0.5,'npc'),y=unit(yp,'npc'),just<-c('left','center'))
+                grid.lines(x=unit(c(0.43,0.49),'npc'),
+                           y=unit.c(unit(with.by.name[[w[sti]]],'native'),
+                                    unit(yp,'npc')),
+                           gp=gp)
+             }
+
+
+          popViewport()
+       popViewport()
+    popViewport()
+dev.off()
+
---
+++ videos/volcano.1way+obs.R
@@ -0,0 +1,285 @@
+#!/usr/common/graphics/R/R-3.1.1/bin/Rscript --no-save
+
+# Pressures and temperatures from 20CR2c for 1815-17
+# Show 24-hour running mean temperatures (avoids diurnal cycle problem).
+
+# Shows version 3.6.6 (Europe only)
+
+library(GSDF.TWCR)
+library(GSDF.WeatherMap)
+library(parallel)
+library(getopt)
+library(chron)
+
+opt = getopt(c(
+  'year',   'y', 2, "integer",
+  'month',   'm', 2, "integer",
+  'nmonths','n', 2, "integer",
+  'obs', 'o', 2, 'character'
+));
+if ( is.null(opt$year  ) )   { stop("Year not specified") }
+if ( is.null(opt$month  ) )   { stop("Month not specified") }
+if ( is.null(opt$nmonths ) ) { opt$nmonths  = 6 }
+if ( is.null(opt$obs ) ) { opt$obs='T' }
+
+# Useful subset of stations for temperature comparisons
+good.stations<-c(2,3,4,7,8,13,16,18,19,20,22,24,25,26,27,28,33,36,46,47,49,50,51)
+#load('../Yuri/0_all_station_and_travel_pressure_data.Rdata')
+load('Station.anomalies.Rdata')
+
+Year<-opt$year
+Month<-opt$month
+Day<-1
+Hour<-0
+n.total<-opt$nmonths*31*24 # Total number of hours to be rendered
+fog.threshold<-exp(1)
+
+GSDF.cache.dir<-sprintf("%s/GSDF.cache",Sys.getenv('SCRATCH'))
+if(!file.exists(GSDF.cache.dir)) dir.create(GSDF.cache.dir,recursive=TRUE)
+Imagedir<-sprintf("%s/images/Tambora.1way+obs",Sys.getenv('SCRATCH'))
+if(!file.exists(Imagedir)) dir.create(Imagedir,recursive=TRUE)
+
+c.date<-chron(dates=sprintf("%04d/%02d/%02d",Year,Month,Day),
+          times=sprintf("%02d:00:00",Hour),
+          format=c(dates='y/m/d',times='h:m:s'))
+
+Options<-WeatherMap.set.option(NULL)
+
+Options<-WeatherMap.set.option(Options,'lat.min',-20)
+Options<-WeatherMap.set.option(Options,'lat.max',20)
+Options<-WeatherMap.set.option(Options,'lon.min',-35)
+Options<-WeatherMap.set.option(Options,'lon.max',35)
+Options<-WeatherMap.set.option(Options,'pole.lon',195)
+Options<-WeatherMap.set.option(Options,'pole.lat',35)
+Options$label.yp<-0.03
+Options$mslp.base=0                    # Base value for anomalies
+#Options$mslp.base=101325                    # Base value for anomalies
+Options$mslp.range=50000                    # Anomaly for max contour
+Options$mslp.step=500                       # Smaller -> more contours
+Options$mslp.tpscale=500                    # Smaller -> contours less transparent
+Options$mslp.lwd=2
+Options<-WeatherMap.set.option(Options,'obs.size',0.5)
+Options<-WeatherMap.set.option(Options,'obs.colour',rgb(255,215,0,255,
+                                                       maxColorValue=255))
+
+Draw.obs<-function(year,month,day,hour,Options,Trange=10,size=0.015) {
+   od<-chron(dates=sprintf("%04d/%02d/%02d",year,month,day),
+          times=sprintf("%02d:00:00",hour),
+          format=c(dates='y/m/d',times='h:m:s'))
+   for(station in good.stations) {
+      w<-which(abs(as.numeric(Station.anomalies$dates)-as.numeric(od))<0.125 &
+               !is.na(Station.anomalies$t2m[[station]]$obs))
+      if(length(w)<1) {
+         w<-which(abs(as.numeric(Station.anomalies$dates)-as.numeric(od))<0.25 &
+                  !is.na(Station.anomalies$t2m[[station]]$obs))
+      }
+      if(length(w)<1) {
+         w<-which(abs(as.numeric(Station.anomalies$dates)-as.numeric(od))<0.5 &
+                  !is.na(Station.anomalies$t2m[[station]]$obs))
+      }
+      if(length(w)==0) next
+      t2m<-Station.anomalies$t2m[[station]]$obs[w[1]]
+      Lat<-Station.anomalies$latitude[[station]]
+      Lon<-Station.anomalies$longitude[[station]]
+      if(is.na(t2m) | is.na(Lat) | is.na(Lon)) next
+      if(Options$pole.lon!=0 || Options$pole.lat!=90) {
+           l2<-GSDF.ll.to.rg(Lat,Lon,Options$pole.lat,Options$pole.lon)
+           Lon<-l2$lon
+           Lat<-l2$lat
+      }
+      spread<-Station.anomalies$t2m[[station]]$v366.spread[w[1]]
+      t.diff<-t2m-Station.anomalies$t2m[[station]]$v366.mean[w[1]]
+      #if(abs(t.diff/spread)>3) col=rgb(1,1,1,1)
+      col=rgb(0,0,0,1)
+      if(t2m>0) fill=rgb(1,0,0,min(0.99,t2m/Trange))
+      else fill=rgb(0,0,1,min(0.99,-1*t2m/Trange))
+      gp<-gpar(col=col,fill=fill,lwd=1)
+      grid.points(x=unit(Lon,'native'),
+                  y=unit(Lat,'native'),
+                  pch=21,
+                  size=unit(size,'snpc'),
+                  gp=gp)
+   }
+}
+
+
+
+Options$ice.points<-50000
+land<-WeatherMap.get.land(Options)
+
+next.day<-function(year,month,day) {
+  c.date<-chron(dates=sprintf("%04d/%02d/%02d",year,month,day),
+          times=sprintf("%02d:00:00",12),
+          format=c(dates='y/m/d',times='h:m:s'))+1
+  return(c(as.integer(as.character(years(c.date))),
+           as.integer(months(c.date)),
+           as.integer(days(c.date))))
+}
+previous.day<-function(year,month,day) {
+  c.date<-chron(dates=sprintf("%04d/%02d/%02d",year,month,day),
+          times=sprintf("%02d:00:00",12),
+          format=c(dates='y/m/d',times='h:m:s'))-1
+  return(c(as.integer(as.character(years(c.date))),
+           as.integer(months(c.date)),
+           as.integer(days(c.date))))
+}
+  
+
+get.temperature.daily<-function(year,month,day,hour,version,type='mean') {
+
+   date.range<-c('','')
+   if(hour<12) {
+      pd<-previous.day(year,month,day)
+      date.range<-c(sprintf("%04d-%02d-%02d:%02d",pd[1],pd[2],pd[3],hour+12),
+                    sprintf("%04d-%02d-%02d:%02d",year,month,day,hour+12))
+   }
+   if(hour>=12) {
+      nd<-next.day(year,month,day)
+      date.range<-c(sprintf("%04d-%02d-%02d:%02d",year,month,day,hour-12),
+                    sprintf("%04d-%02d-%02d:%02d",nd[1],nd[2],nd[3],hour-12))
+   }
+
+   t<-TWCR.get.slab.from.hourly('air.2m',date.range=date.range,version=version,type=type) 
+   t$data<-apply(t$data,c(1,2),mean) # average over the times
+   t$dimensions[[3]]$values<-mean(t$dimensions[[3]]$values)
+   return(t)
+}
+
+Draw.temperature<-function(temperature,Options,Trange=10) {
+  
+  Options.local<-Options
+  Options.local$fog.min.transparency<-0.5
+  tplus<-temperature
+  tplus$data[]<-pmax(1,pmin(Trange,tplus$data))/Trange
+  Options.local$fog.colour<-c(1,0,0)
+  WeatherMap.draw.fog(tplus,Options.local)
+  tminus<-temperature
+  tminus$data[]<-tminus$data*-1
+  tminus$data[]<-pmax(1,pmin(Trange,tminus$data))/Trange
+  Options.local$fog.colour<-c(0,0,1)
+  WeatherMap.draw.fog(tminus,Options.local)
+}
+
+
+
+Draw.pressure<-function(mslp,Options,colour=c(0,0,0)) {
+
+  M<-GSDF.WeatherMap:::WeatherMap.rotate.pole(mslp,Options)
+  lats<-M$dimensions[[GSDF.find.dimension(M,'lat')]]$values
+  longs<-M$dimensions[[GSDF.find.dimension(M,'lon')]]$values
+    # Need particular data format for contourLines
+  if(lats[2]<lats[1] || longs[2]<longs[1] || max(longs) > 180 ) {
+    if(lats[2]<lats[1]) lats<-rev(lats)
+    if(longs[2]<longs[1]) longs<-rev(longs)
+    longs[longs>180]<-longs[longs>180]-360
+    longs<-sort(longs)
+    M2<-M
+    M2$dimensions[[GSDF.find.dimension(M,'lat')]]$values<-lats
+    M2$dimensions[[GSDF.find.dimension(M,'lon')]]$values<-longs
+    M<-GSDF.regrid.2d(M,M2)
+  }
+  z<-matrix(data=M$data,nrow=length(longs),ncol=length(lats))
+  contour.levels<-seq(Options$mslp.base-Options$mslp.range,
+                      Options$mslp.base+Options$mslp.range,
+                      Options$mslp.step)
+  lines<-contourLines(longs,lats,z,
+                       levels=contour.levels)
+  if(!is.na(lines) && length(lines)>0) {
+     for(i in seq(1,length(lines))) {
+         tp<-min(1,(abs(lines[[i]]$level-Options$mslp.base)/
+                    Options$mslp.tpscale))
+         lt<-2
+         lwd<-1
+         if(lines[[i]]$level<=Options$mslp.base) {
+             lt<-1
+             lwd<-1
+         }
+         gp<-gpar(col=rgb(colour[1],colour[2],colour[3],tp),
+                             lwd=Options$mslp.lwd*lwd,lty=lt)
+         grid.xspline(x=unit(lines[[i]]$x,'native'),
+                    y=unit(lines[[i]]$y,'native'),
+                    shape=1,
+                    gp=gp)
+     }
+  }
+}
+
+plot.hour<-function(year,month,day,hour) {    
+
+    image.name<-sprintf("%04d-%02d-%02d:%02d.png",year,month,day,hour)
+
+    ifile.name<-sprintf("%s/%s",Imagedir,image.name)
+    if(file.exists(ifile.name) && file.info(ifile.name)$size>0) return()
+
+    t2m.normal<-get.temperature.daily(year,month,day,hour,version='3.5.4',
+                                             type='normal')
+    prmsl.sd<-TWCR.get.slice.at.hour('prmsl',year,month,day,hour,
+                                         version='3.4.1',type='standard.deviation')
+    prmsl.normal<-TWCR.get.slice.at.hour('prmsl',year,month,day,hour,version='3.5.4',
+                                             type='normal')
+    if(opt$obs=='T') {
+       obs<-TWCR.get.obs(year,month,day,hour,version='3.5.4')
+       w<-which(obs$Longitude>180)
+       obs$Longitude[w]<-obs$Longitude[w]-360
+    }
+
+     png(ifile.name,
+             width=1080*16/9,
+             height=1080,
+             bg='white',
+             pointsize=24,
+             type='cairo')
+
+
+	prmsl<-TWCR.get.slice.at.hour('prmsl',year,month,day,hour,version='3.6.6')
+	prmsl.spread<-TWCR.get.slice.at.hour('prmsl',year,month,day,hour,version='3.6.6',
+						  type='spread')
+	fog<-TWCR.relative.entropy(prmsl.normal,prmsl.sd,prmsl,prmsl.spread)
+	fog$data[]<-1-pmin(fog.threshold,pmax(0,fog$data))/fog.threshold
+	t2m<-get.temperature.daily(year,month,day,hour,version='3.6.6')
+	prmsl$data[]<-prmsl$data-prmsl.normal$data
+	t2m$data[]<-t2m$data-t2m.normal$data
+
+	   pushViewport(viewport(width=1.0,height=1.0,x=0.0,y=0.0,
+			      just=c("left","bottom"),clip='on'))
+
+	   pushViewport(dataViewport(c(Options$lon.min,Options$lon.max),
+				     c(Options$lat.min,Options$lat.max),
+				      extension=0))
+              grid.polygon(x=unit(c(0,1,1,0),'npc'),y=unit(c(0,0,1,1),'npc'),
+                           gp=gpar(col=Options$sea.colour,fill=Options$sea.colour))
+	      ip<-WeatherMap.rectpoints(Options$ice.points,Options)
+	      #WeatherMap.draw.ice(ip$lat,ip$lon,icec,Options)
+	      WeatherMap.draw.land(land,Options)
+
+	    Draw.temperature(t2m,Options)
+	    if(opt$obs=='T') Draw.obs(year,month,day,hour,Options,Trange=10,size=0.025)
+	    Draw.pressure(prmsl,Options,colour=c(0,0,0))
+	    WeatherMap.draw.fog(fog,Options)
+
+	    Options$label<-sprintf("%04d-%02d-%02d:%02d",year,month,day,hour)
+	    WeatherMap.draw.label(Options)
+	 upViewport()
+	 upViewport()
+
+
+    dev.off()
+}
+
+for(n.count in seq(0,n.total)) {
+
+    n.date<-c.date+n.count/24
+    year<-as.numeric(as.character(years(n.date)))
+    month<-months(n.date)
+    day<-days(n.date)
+    hour<-n.count%%24
+
+    image.name<-sprintf("%04d-%02d-%02d:%02d.png",year,month,day,hour)
+    ifile.name<-sprintf("%s/%s",Imagedir,image.name)
+    if(file.exists(ifile.name) && file.info(ifile.name)$size>0) next
+    # Each plot in a seperate parallel process
+    mcparallel(plot.hour(year,month,day,hour))
+    if(hour==20) mccollect(wait=TRUE)
+
+}
+mccollect()
